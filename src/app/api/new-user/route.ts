53054dde2a144a4fd19f084cb919672d1ad6d0b2
Julian Flores <julianfloresdev@gmail.com>
2023-11-06 19:04:11 -0300

    Refactor new user API route to use separate post handler.

--- src/app/api/new-user/route.ts
+++ src/app/api/new-user/route.ts
@@ -2,26 +2,27 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Contact } from '@/interfaces/interfaces';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const API_URL = process.env.API_URL || '';
-    if (req.method === 'POST') {
-        const data: Contact = req.body;
-        try {
-            const response = await fetch(API_URL, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${process.env.API_TOKEN}`
-                },
-                body: JSON.stringify(data)
-            });
-            const result = await response.json();
-            res.status(200).json(result);
-        } catch (error) {
-            res.status(500).json({ error: 'Error creating user' });
-        }
-    } else {
-        res.setHeader('Allow', ['POST'])
-        res.status(405).end(`Method ${req.method} Not Allowed`)
+const API_URL = process.env.API_URL || '';
+
+async function postHandler(req: NextApiRequest, res: NextApiResponse) {
+    const data: Contact = req.body;
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.API_TOKEN}`
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({ error: 'Error creating user' });
     }
 }
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    POST: postHandler,
+};
